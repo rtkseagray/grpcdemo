88eee0b64f490879fd76896dfe401a15d1c2ed14
Warren Gray <warren.gray@arcticwolf.com>
2020-01-08 21:49:49 -0500

    Add parameter to existing request.

--- server/server.go
+++ server/server.go
@@ -11,6 +11,7 @@ type DemoService struct {
 	rpc.UnimplementedDemoServiceServer
 }
 
+// Note that the signature of our handler didn't change when we regenerated the server code.
 func (*DemoService) HelloWorld(_ context.Context, req *rpc.HelloWorldRequest) (*rpc.HelloWorldResponse, error) {
 	return &rpc.HelloWorldResponse{Greeting: fmt.Sprintf("Hi, %s!", req.Name)}, nil
 }

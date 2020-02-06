db9b4431775bff17cb3eb4c6f816f3a476e693d2
Warren Gray <warren.gray@arcticwolf.com>
2020-02-06 09:19:13 -0500

    Unit testing!

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.2
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 	google.golang.org/grpc v1.26.0
 )
---
+++ server/server_test.go
@@ -0,0 +1,88 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+
+	"grpcdemo/rpc"
+)
+
+type ServerTestSuite struct {
+	suite.Suite
+}
+
+func TestServerTestSuite(t *testing.T) {
+	suite.Run(t, &ServerTestSuite{})
+}
+
+// Testing unary methods are fairly straightforward. Simply construct a request object, call your
+// handler endpoint, and verify the response.
+func (s *ServerTestSuite) TestHelloWorld() {
+	server := &DemoService{}
+	request := rpc.HelloWorldRequest{Name: "Warren", NickName: "Seagray"}
+	response, err := server.HelloWorld(context.Background(), &request)
+
+	s.Assert().NoError(err)
+	s.Assert().Equal(
+		fmt.Sprintf("Hi, %s! Should I call you %s?", request.Name, request.NickName),
+		response.Greeting,
+	)
+}
+
+// Streaming RPCs are more complicated to test. This is an example of using gRPC's buffcon package.
+// buffcon implements an in-memory full-duplex network connection using channels. We can start the
+// server on a buffcon listener and then connect the generated client code to it to verify the
+// server handlers.
+func (s *ServerTestSuite) TestSpellMyName() {
+
+	// Create the server and bind it to the buffcon.
+	listener := bufconn.Listen(3)
+	server := grpc.NewServer()
+	rpc.RegisterDemoServiceServer(server, &DemoService{})
+	go func() {
+		err := server.Serve(listener)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	defer server.Stop()
+
+	// Connect our generated gRPC client to the server.
+	conn, err := grpc.Dial(
+		"",
+		grpc.WithContextDialer(
+			func(ctx context.Context, s string) (conn net.Conn, err error) {
+				return listener.Dial()
+			},
+		),
+		grpc.WithInsecure(),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	client := rpc.NewDemoServiceClient(conn)
+
+	// Create a request and send it to the server.
+	request := rpc.HelloWorldRequest{Name: "AB"}
+	stream, err := client.SpellMyName(context.Background(), &request)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, l := range request.Name {
+		recv, err := stream.Recv()
+		s.Assert().NoError(err)
+		s.Assert().Equal(string(l), recv.Letter)
+	}
+
+	_, err = stream.Recv()
+	s.Assert().Equal(io.EOF, err)
+}
